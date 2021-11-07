c0749d1415170b23dd40ce334a4d4fa4b240d723
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-11-07 12:33:38 +0900

    Fixed sample/psg_play; changed to use `rest` command instead of `volume 0`

--- sample/psg_play/src/psg_play.c
+++ sample/psg_play/src/psg_play.c
@@ -2,12 +2,15 @@
 
 #include <msx.h>
 
+// see also https://mori0091.github.io/libmsx/structsound__clip.html
+//      and https://mori0091.github.io/libmsx/sound_8h.html
+
 // sound data stream for PSG channel A
 const uint8_t channel_1[] ={
   0x60, 0x1e, 0x88, 0x01, 0xac, // T120L4 V8 C
   0x40, 0x1e, 0x01, 0x7d,       // T120L4    D
   0x40, 0x1e, 0x01, 0x53,       // T120L4    E
-  0x20, 0x1e, 0x80,             // T120L4    R
+  0x20, 0x1e, 0x10,             // T120L4    R
   0xff,                         // end of stream
 };
 // sound data stream for PSG channel B
@@ -15,7 +18,7 @@ const uint8_t channel_2[] = {
   0x60, 0x1e, 0x88, 0x00, 0xd6, // T120L4 V8 O5C
   0x40, 0x1e, 0x00, 0xbe,       // T120L4    O5D
   0x40, 0x1e, 0x00, 0xaa,       // T120L4    O5E
-  0x20, 0x1e, 0x80,             // T120L4 R
+  0x20, 0x1e, 0x10,             // T120L4    R
   0xff,                         // end of stream
 };
 // sound data stream for PSG channel C
@@ -23,7 +26,7 @@ const uint8_t channel_3[] = {
   0x60, 0x1e, 0x88, 0x02, 0x3b, // T120L4 V8 O3G
   0x40, 0x1e, 0x01, 0xfd,       // T120L4    O3A
   0x40, 0x1e, 0x01, 0xc5,       // T120L4    O3B
-  0x20, 0x1e, 0x80,             // T120L4 R
+  0x20, 0x1e, 0x10,             // T120L4    R
   0xff,                         // end of stream
 };
 

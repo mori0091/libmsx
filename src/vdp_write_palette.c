267e4d3b146aaaebd8248e92cb3653f70b7742d9
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-11-09 20:19:08 +0900

    Fixed "Attempt to cast away const"

--- src/vdp_write_palette.c
+++ src/vdp_write_palette.c
@@ -16,7 +16,7 @@
 #include "vdp_internal.h"
 
 void vdp_write_palette(const palette_t palettes[16]) {
-  uint8_t* p = (uint8_t*)palettes;
+  const uint8_t* p = (const uint8_t*)palettes;
   __critical {
     VDP_SET_CTRL(16, 0);
     for (uint8_t i = 16; i--; ) {

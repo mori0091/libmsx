e61cad09302eb77caa216fb8f5643611d8617a98
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-11-21 09:45:29 +0900

    Added `msx_set_cpu_mode()`

--- include/bios.h
+++ include/bios.h
@@ -18,7 +18,6 @@
 
 #include <stdint.h>
 
-// Main ROM
 /**
  * Major version code of MSX BIOS.
  */
@@ -52,6 +51,32 @@ inline uint8_t msx_get_vsync_frequency(void) {
   return ((INTERNATIONAL_ID_1 & 0x80) ? 50 : 60);
 }
 
-// Sub ROM
+/**
+ * Change the CPU mode based on the given `mode`.
+ *
+ * \note
+ * - This function is the C interface to the `CHGCPU (0180H/MAIN)` BIOS function
+ *   for MSXturboR.
+ * - In case of MSX, MSX2, or MSX2+, this function does nothing.
+ *
+ *
+ * The parameter `mode` shall be the following bit pattern:
+ * - bit #7     : LED
+ * - bit #6..#2 : (unused, shall be all '0')
+ * - bit #1..#0 : CPU
+ *
+ * **LED**
+ * - `0` : Do not change the LED status
+ * - `1` : Turn the LED on and off according to the **CPU**
+ *
+ * **CPU**
+ * - `00` : Z80 mode
+ * - `01` : R800 ROM mode
+ * - `10` : R800 DRAM mode
+ * - `11` : (reserved)
+ *
+ * \param mode    cpu mode
+ */
+void msx_set_cpu_mode(const uint8_t mode);
 
 #endif
---
+++ src/msx_set_cpu_mode.c
@@ -0,0 +1,24 @@
+// -*- coding: utf-8-unix -*-
+/**
+ * \file msx_set_cpu_mode.c
+ *
+ * Copyright (c) 2021 Daishi Mori (mori0091)
+ *
+ * This software is released under the MIT License.
+ * See https://github.com/mori0091/libmsx/blob/main/LICENSE
+ *
+ * GitHub libmsx project
+ * https://github.com/mori0091/libmsx
+ */
+
+#include "../include/bios.h"
+
+void msx_set_cpu_mode(const uint8_t mode) {
+  ((void)mode);
+  if (msx_get_version() < 3) return;
+  __asm__("ld hl, #2+0");
+  __asm__("add hl, sp");
+  __asm__("ld a, (hl)");
+  __asm__("and a, #0x83");
+  __asm__("jp _CHGCPU");
+}

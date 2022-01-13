883081d4d7becbeb118348eef915ee24de6ca01a
mori0091 <mori-d@qc4.so-net.ne.jp>
2022-01-13 20:50:17 +0900

    Refactored `msx_set_cpu_mode()`.

--- src/msx_set_cpu_mode.c
+++ src/msx_set_cpu_mode.c
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2021 Daishi Mori (mori0091)
  *
- * This software is released under the MIT License.
+ * This software is released under the MIT License.\n
  * See https://github.com/mori0091/libmsx/blob/main/LICENSE
  *
  * GitHub libmsx project
@@ -13,14 +13,7 @@
 
 #include "../include/bios.h"
 
-static __at(0x0180) uint8_t CHGCPU[0];
-
 void msx_set_cpu_mode(const uint8_t mode) {
-  ((void)mode);
   if (msx_get_version() < 3) return;
-  __asm__("ld hl, #2+0");
-  __asm__("add hl, sp");
-  __asm__("ld a, (hl)");
-  __asm__("and a, #0x83");
-  __asm__("jp _CHGCPU");
+  msx_CHGCPU(mode);
 }

f47e440909891344594aa4365ce7d4185b6484a9
mori0091 <mori-d@qc4.so-net.ne.jp>
2022-03-16 08:28:57 +0900

    Fixed to do not use __critical function.
    Because __critical function of SDCC 4.2.0 seems buggy when __sdcccall(1)
    calling convention is used.

--- src/ay_3_8910.c
+++ src/ay_3_8910.c
@@ -23,24 +23,26 @@ void ay_3_8910_init(void) {
   ay_3_8910_play();
 }
 
-void ay_3_8910_play(void) __critical {
-  {
-    uint8_t x = ay_3_8910_buffer[7];
-    x &= 0x3f;
-    x |= 0x80;
-    ay_3_8910_buffer[7] = x;
+void ay_3_8910_play(void) {
+  __critical {
+    {
+      uint8_t x = ay_3_8910_buffer[7];
+      x &= 0x3f;
+      x |= 0x80;
+      ay_3_8910_buffer[7] = x;
+    }
+    uint8_t * p = ay_3_8910_buffer;
+    uint8_t reg = 0;
+    /* R#0..R#10 */
+    while (reg < 11) {
+      psg_set(reg++, *p++);
+    }
+    /* R#11..R#13 */
+    if (ay_3_8910_buffer[13] < 16) {
+      psg_set(reg++, *p++);
+      psg_set(reg++, *p++);
+      psg_set(reg++, *p++);
+    }
+    ay_3_8910_buffer[13] = 0xff;
   }
-  uint8_t * p = ay_3_8910_buffer;
-  uint8_t reg = 0;
-  /* R#0..R#10 */
-  while (reg < 11) {
-    psg_set(reg++, *p++);
-  }
-  /* R#11..R#13 */
-  if (ay_3_8910_buffer[13] < 16) {
-    psg_set(reg++, *p++);
-    psg_set(reg++, *p++);
-    psg_set(reg++, *p++);
-  }
-  ay_3_8910_buffer[13] = 0xff;
 }

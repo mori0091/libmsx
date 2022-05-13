dff94957f8411510a34ffae6738e991ee7dcd946
mori0091 <mori-d@qc4.so-net.ne.jp>
2022-05-14 04:55:21 +0900

    refactor(crt0): moved "init" to CODE section
    NOTE: CODE section now starts from 0x4010.

--- crt0/16k.4000/crt0.s
+++ crt0/16k.4000/crt0.s
@@ -11,7 +11,7 @@
 ;;; https://github.com/mori0091/libmsx
 
 ;;; crt0 for MSX ROM of 16KB starting at 0x4000
-;;; suggested options: --code-loc 0x4020 --data-loc 0xc000
+;;; suggested options: --code-loc 0x4010 --data-loc 0xc000
 ;;; `main` should be `void main(void)`
 ;;; `return` from `main` causes soft reset.
 
@@ -21,7 +21,6 @@
         .globl  _libmsx___init_intr
 
         HIMEM  = 0xfc4a
-        H_STKE = 0xfeda
 
         .area   _HEADER (ABS)
         ;; ROM header
@@ -37,14 +36,6 @@
         .dw     0x0000
         .dw     0x0000
 
-init:
-init2:
-        ld      sp,(HIMEM)
-        call    gsinit
-        call    _libmsx___init_intr
-        call    _main
-        jp      _exit
-
         ;; ---- ordering segments for linker
         .area   _HOME
         .area   _CODE
@@ -59,11 +50,13 @@ init2:
         ;; ----
 
 	.area   _CODE
+init:
+        ld      sp,(HIMEM)
+        call    gsinit
+        call    _libmsx___init_intr
+        call    _main
 _exit::
         rst     0x00
-1$:
-        halt
-        jr      1$
 
         ;;
         .area   _GSINIT
@@ -85,13 +78,13 @@ gsinit::
         ldir
 zeroed_data:
         ;; initialize global variables that have explicit initializer
-	ld	bc, #l__INITIALIZER
-	ld	a, b
-	or	a, c
-	jr	Z, gsinit_next
-	ld	de, #s__INITIALIZED
-	ld	hl, #s__INITIALIZER
-	ldir
+        ld      bc, #l__INITIALIZER
+        ld      a, b
+        or      a, c
+        jr      Z, gsinit_next
+        ld      de, #s__INITIALIZED
+        ld      hl, #s__INITIALIZER
+        ldir
 gsinit_next:
-	.area   _GSFINAL
-	ret
+        .area   _GSFINAL
+        ret
--- crt0/32k.4000/crt0.s
+++ crt0/32k.4000/crt0.s
@@ -11,7 +11,7 @@
 ;;; https://github.com/mori0091/libmsx
 
 ;;; crt0 for MSX ROM of 32KB starting at 0x4000
-;;; suggested options: --code-loc 0x4020 --data-loc 0xc000
+;;; suggested options: --code-loc 0x4010 --data-loc 0xc000
 ;;; `main` should be `void main(void)`
 ;;; `return` from `main` causes soft reset.
 
@@ -20,8 +20,12 @@
         .globl  _exit
         .globl  _libmsx___init_intr
 
-        HIMEM  = 0xfc4a
-        H_STKE = 0xfeda
+        RSLREG = 0x0138         ; Read SLot selector REGister
+        ENASLT = 0x0024         ; ENAble SLoT
+
+        EXPTBL = 0xfcc1         ; (4 bytes) Extended flag table of 4 primary slots
+        SLTTBL = 0xfcc5         ; (4 bytes) Save area for secondary slot selector registers
+        HIMEM  = 0xfc4a         ; (2 bytes) Pointer to upper limit address of free area
 
         .area   _HEADER (ABS)
         ;; ROM header
@@ -37,34 +41,28 @@
         .dw     0x0000
         .dw     0x0000
 
-init:
-init2:
-        ld      sp,(HIMEM)
-        call    find_rom_page_2
-        call    gsinit
-        call    _libmsx___init_intr
-        call    _main
-        jp      _exit
-
         ;; ---- ordering segments for linker
         .area   _HOME
         .area   _CODE
         .area   _INITIALIZER
         .area   _GSINIT
-	.area   _GSFINAL
-	.area	_DATA
-	.area	_INITIALIZED
-	.area	_BSEG
-	.area   _BSS
-	.area   _HEAP
+        .area   _GSFINAL
+        .area   _DATA
+        .area   _INITIALIZED
+        .area   _BSEG
+        .area   _BSS
+        .area   _HEAP
         ;; ----
 
-	.area   _CODE
+        .area   _CODE
+init:
+        ld      sp, (HIMEM)
+        call    find_rom_page_2
+        call    gsinit
+        call    _libmsx___init_intr
+        call    _main
 _exit::
         rst     0x00
-1$:
-        halt
-        jr      1$
 
         ;;
 ;------------------------------------------------
@@ -82,45 +80,45 @@ _exit::
 ; Comprobacion de RAM/ROM
 
 find_rom_page_2::
-	ld hl, #0x4000
-	ld b, (hl)
-	xor a
-	ld (hl), a
-	ld a, (hl)
-	or a
-	jr nz,5$ ; jr nz,@@ROM
-	; El programa esta en RAM - no buscar
-	ld (hl),b
-	ret
+        ld      hl, #0x4000
+        ld      b, (hl)
+        xor     a
+        ld      (hl), a
+        ld      a, (hl)
+        or      a
+        jr      nz,5$ ; jr nz,@@ROM
+        ; El programa esta en RAM - no buscar
+        ld      (hl), b
+        ret
 5$: ; ----------- @@ROM:
-	di
-	; Slot primario
-	call #0x0138 ; call RSLREG
-	rrca
-	rrca
-	and #0x03
-	; Slot secundario
-	ld c, a
-	ld hl, #0xfcc1
-	add a, l
-	ld l, a
-	ld a, (hl)
-	and #0x80
-	or c
-	ld c, a
-	inc l
-	inc l
-	inc l
-	inc l
-	ld a, (hl)
-	; Definir el identificador de slot
-	and #0x0c
-	or c
-	ld h, #0x80
-	; Habilitar permanentemente
-	call #0x0024 ; call ENASLT
-	ei
-	ret
+        di
+        ; Slot primario
+        call    RSLREG
+        rrca
+        rrca
+        and     #0x03
+        ; Slot secundario
+        ld      c, a
+        ld      hl, #EXPTBL
+        add     a, l
+        ld      l, a
+        ld      a, (hl)
+        and     #0x80
+        or      c
+        ld      c, a
+        inc     l
+        inc     l
+        inc     l
+        inc     l
+        ld      a, (hl)
+        ; Definir el identificador de slot
+        and     #0x0c
+        or      c
+        ld      h, #0x80
+        ; Habilitar permanentemente
+        call    ENASLT
+        ei
+        ret
 ;------------------------------------------------
         .area   _GSINIT
 gsinit::
@@ -141,13 +139,13 @@ gsinit::
         ldir
 zeroed_data:
         ;; initialize global variables that have explicit initializer
-	ld	bc, #l__INITIALIZER
-	ld	a, b
-	or	a, c
-	jr	Z, gsinit_next
-	ld	de, #s__INITIALIZED
-	ld	hl, #s__INITIALIZER
-	ldir
+        ld      bc, #l__INITIALIZER
+        ld      a, b
+        or      a, c
+        jr      Z, gsinit_next
+        ld      de, #s__INITIALIZED
+        ld      hl, #s__INITIALIZER
+        ldir
 gsinit_next:
-	.area   _GSFINAL
-	ret
+        .area   _GSFINAL
+        ret

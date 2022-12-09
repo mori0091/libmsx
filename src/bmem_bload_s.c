212c5b9348e2fd88c4e87f8b3587d41078dc7670
mori0091 <mori-d@qc4.so-net.ne.jp>
2022-12-10 03:02:49 +0900

    feat(bmem): Banked Memory interface for MegaROM
    **MegaROM** is treated as **banked memory** consisting of one large
    address space (or a series of 16KiB segments).
    - Direct access to banked memory by switching page 2.
    - Reading and copying from banked memory.

---
+++ src/bmem_bload_s.c
@@ -0,0 +1,25 @@
+// -*- coding: utf-8-unix -*-
+/*
+ * Copyright (c) 2022 Daishi Mori (mori0091)
+ *
+ * This software is released under the MIT License.\n
+ * See https://github.com/mori0091/libmsx/blob/main/LICENSE
+ *
+ * GitHub libmsx project\n
+ * https://github.com/mori0091/libmsx
+ */
+/**
+ * \file bmem_bload_s.c
+ */
+
+#include "bmem.h"
+
+void bmem_bload_s(bmemptr_t src) {
+  if (bmem_get(src) == 0xfe) {
+    uint16_t beg = bmem_get_u16(src+1);
+    uint16_t end = bmem_get_u16(src+3);
+    if (beg < end) {
+      bmem_copy_to_vmem(src+7, (vmemptr_t)beg, (uint32_t)end - beg + 1);
+    }
+  }
+}
---
+++ src/bmem_get.c
@@ -0,0 +1,24 @@
+// -*- coding: utf-8-unix -*-
+/*
+ * Copyright (c) 2022 Daishi Mori (mori0091)
+ *
+ * This software is released under the MIT License.\n
+ * See https://github.com/mori0091/libmsx/blob/main/LICENSE
+ *
+ * GitHub libmsx project\n
+ * https://github.com/mori0091/libmsx
+ */
+/**
+ * \file bmem_get.c
+ */
+
+#include "bmem.h"
+
+uint8_t bmem_get(bmemptr_t src) {
+  const uint8_t old_bank = bmem_get_bank();
+  uint8_t bank = bmem_bank_of(src);
+  bmem_set_bank(bank);
+  uint8_t ret = *(const uint8_t *)(const uintptr_t)((src & 0x3fff) + 0x8000);
+  bmem_set_bank(old_bank);
+  return ret;
+}

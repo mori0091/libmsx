584c3ab5d8209ef6c03a0479c99039ef5d70b339
Daishi Mori <mori-d@qc4.so-net.ne.jp>
2023-01-25 09:25:02 +0900

    Refactor(sample/driving) (#43)
    * fix(sample/driving): VRAM was not zero cleared.
    
    * fix(sample/driving): curve shall be smoothly
    
    * fix(sample/driving): for time critical robustness
    
    * refactor(sample/driving): refactor code, and fixed constants
    
    * feat(sample/driving): add vehicle sprite
    
    * chore(sample/driving): modified some constants

--- sample/driving/src/driving.c
+++ sample/driving/src/driving.c
@@ -34,50 +34,80 @@
 #define LINES          (212)    // Height of the screen
 
 #define HSYNC_LINE0    (20)     // Top line of the drawing area for distant landscape
-#define HORIZON_LINE   (108)    // Top line of the road / Line of the horizon.
+#define HORIZON_LINE   (LINES - RASTER_LINES) // Top line of the road / Line of the horizon.
 
-#define RASTER_LINES   (212 - HORIZON_LINE)
-#define RASTER_ROWS    ((RASTER_LINES + 2) / 3)
+#define RASTER_LINES   (93)     // must be multiple of three.
+#define RASTER_ROWS    (RASTER_LINES / 3)
 
-#define CENTER         (128)    // scroll amount of the +/- zero position
+#define CENTER             (128) // scroll amount of the +/- zero position
+#define HALF_WIDTH_OF_ROAD (192)
 
-#define MAX_R          (82)     // maximum curvature (8x)
-#define MAX_POS_X      (MAX_R * 10) // upper limit of the vehicle's lateral position (8x)
+// maximum curvature
+#define MAX_R              ((192L << 7) * 2 / RASTER_ROWS / RASTER_ROWS)
+// upper limit of the vehicle's lateral position
+#define MAX_POS_X          (((HALF_WIDTH_OF_ROAD + 12) << 7) / RASTER_ROWS)
 
-static bool start_raster;
-static uint8_t idx;
-static uint16_t scroll_x[2][RASTER_ROWS];
-static uint16_t * p_scroll_x;
-static uint16_t bg_scroll_x;
-static uint16_t bg_scroll_x_d;
+static bool start_raster; // flag used in HSYNC interrupt routine for switching its proccess
+static uint8_t idx;       // current index of the `scroll_table` (0 or 1)
+static struct scroll_table {
+  uint8_t r26;         // horizontal scroll amount (towards left by 8 dot unit)
+  uint8_t r27;         // horizontal scroll amount (towards right by 1 dot unit)
+} scroll_table[2][RASTER_ROWS]; // double buffered scroll table
 
-static int8_t R;                // target curvature (8x)
-static int8_t r;                // current curvature (8x)
+static uint16_t bg_scroll_x;    // scroll amount of landscape (integer part)
+static uint16_t bg_scroll_x_d;  // scroll amount of landscape (decimal part)
+
+static int8_t R;                // target lateral road curvature
+static int8_t r;                // the lateral road curvature at the horizon
+static int8_t r0;               // the lateral road curvature at vehicle position
+static int8_t curvatures[RASTER_ROWS]; // ring buffer of lateral road curvatures
+static uint8_t curvatures_idx;         // next element's index of the ring buffer
 
 static int8_t vx;               // vehicle's lateral speed
 static int16_t x_pos;           // vehicle's lateral position (integer part)
 static int16_t x_pos_d;         // vehicle's lateral position (decimal part)
 
+inline uint8_t hscroll_register_r26_value_from(uint16_t x) {
+  return ((uint8_t)((x + 7) >> 3)) & 0x3f;
+}
+
+inline uint8_t hscroll_register_r27_value_from(uint16_t x) {
+  return (-((uint8_t)x)) & 7;
+}
+
 inline void on_vsync(void) {
-  vdp_set_hscroll(0);
+  VDP_SET_CONTROL_REGISTER(26, 0);
+  VDP_SET_CONTROL_REGISTER(27, 0);
   VDP_SET_CONTROL_REGISTER(19, (RG19SA = HSYNC_LINE0 - 6));
   start_raster = false;
 }
 
 inline void on_hsync(void) {
-  p_scroll_x = &scroll_x[idx][0];
   if (!start_raster) {
-    // vdp_set_hscroll(*p_scroll_x);
-    vdp_set_hscroll(bg_scroll_x);
-    VDP_SET_CONTROL_REGISTER(19, (RG19SA = HORIZON_LINE - 6));
+    // scroll the distant landscape area
+    VDP_SET_CONTROL_REGISTER(27, hscroll_register_r27_value_from(bg_scroll_x));
+    VDP_SET_CONTROL_REGISTER(26, hscroll_register_r26_value_from(bg_scroll_x));
+    // setup HSYNC interrupt line to HORIZON_LINE (top of the ground area)
+    VDP_SET_CONTROL_REGISTER(19, (RG19SA = HORIZON_LINE - 4));
     start_raster = true;
     return;
   }
+  // scroll the ground area every 3 lines (raster scroll)
+  const uint8_t * p = (const uint8_t *)scroll_table[idx];
   VDP_SET_STATUS_REGISTER_POINTER(2);
-  for (uint8_t n = 0; n < RASTER_ROWS; n++ ) {
+  for (uint8_t n = RASTER_ROWS; n--; ) {
     while (!(VDP_GET_STATUS_REGISTER_VALUE() & (1 << 5)))
       ;
-    vdp_set_hscroll(*p_scroll_x++);
+    VDP_SET_CONTROL_REGISTER(26, *p++);
+    VDP_SET_CONTROL_REGISTER(27, *p++);
+    while ((VDP_GET_STATUS_REGISTER_VALUE() & (1 << 5)))
+      ;
+    while (!(VDP_GET_STATUS_REGISTER_VALUE() & (1 << 5)))
+      ;
+    while ((VDP_GET_STATUS_REGISTER_VALUE() & (1 << 5)))
+      ;
+    while (!(VDP_GET_STATUS_REGISTER_VALUE() & (1 << 5)))
+      ;
     while ((VDP_GET_STATUS_REGISTER_VALUE() & (1 << 5)))
       ;
   }
@@ -105,14 +135,21 @@ void interrupt_handler(void) {
 }
 
 static void draw_graphics(void) {
+  // Clear page 0 and 1
+  vdp_cmd_execute_HMMV(0,   0, 256, 212, 0);
+  vdp_cmd_execute_HMMV(0, 256, 256, 212, 0);
+
   // Draw landscape (over the horizon)
   {
+    vdp_cmd_execute_HMMV(0,   0, 256, HORIZON_LINE, 0x77);
+    vdp_cmd_execute_HMMV(0, 256, 256, HORIZON_LINE, 0x77);
     uint8_t c = 1;
     for (uint16_t j = 0; j < 256; j += 8) {
+      const uint8_t h = rand() % (HORIZON_LINE - HSYNC_LINE0 - 8) + 8;
       // left half (page 0)
-      vdp_cmd_execute_HMMV(j,   0, 8, HORIZON_LINE, (c << 4) | (c & 15));
+      vdp_cmd_execute_HMMV(j, HORIZON_LINE - h,       8, h, (c << 4) | (c & 15));
       // right half (page 1)
-      vdp_cmd_execute_HMMV(j, 256, 8, HORIZON_LINE, (c << 4) | (c & 15));
+      vdp_cmd_execute_HMMV(j, HORIZON_LINE - h + 256, 8, h, (c << 4) | (c & 15));
       c++;
       if (c > 11) {
         c = 1;
@@ -121,20 +158,20 @@ static void draw_graphics(void) {
   }
 
   // Draw border lines of road
-  for (uint8_t k = 0; k < 8; ++k) {
+  for (int8_t k = -4; k < 4; ++k) {
     // left half (page 0)
-    vdp_cmd_execute_LINE(255, HORIZON_LINE,      64-4+k, 211    , 14, VDP_CMD_IMP);
-    vdp_cmd_execute_LINE(255, HORIZON_LINE,     192-4+k, 211    , 14, VDP_CMD_IMP);
+    vdp_cmd_execute_LINE(255, HORIZON_LINE,     (HALF_WIDTH_OF_ROAD/3)+k, (LINES-1)    , 14, VDP_CMD_IMP);
+    vdp_cmd_execute_LINE(255, HORIZON_LINE,     (HALF_WIDTH_OF_ROAD  )+k, (LINES-1)    , 14, VDP_CMD_IMP);
     // right half (page 1)
-    vdp_cmd_execute_LINE(  0, HORIZON_LINE+256, 192-4+k, 211+256, 14, VDP_CMD_IMP);
-    vdp_cmd_execute_LINE(  0, HORIZON_LINE+256,  64-4+k, 211+256, 14, VDP_CMD_IMP);
+    vdp_cmd_execute_LINE(  0, HORIZON_LINE+256, (HALF_WIDTH_OF_ROAD  )+k, (LINES-1)+256, 14, VDP_CMD_IMP);
+    vdp_cmd_execute_LINE(  0, HORIZON_LINE+256, (HALF_WIDTH_OF_ROAD/3)+k, (LINES-1)+256, 14, VDP_CMD_IMP);
   }
 
   // Draw the ground
   {
     uint8_t c = 0xcc;
     uint8_t h = 4;
-    for (uint8_t i = HORIZON_LINE; i < 212; ) {
+    for (uint8_t i = HORIZON_LINE; i < LINES; ) {
       uint8_t hh = h >> 2;
       // left half (page 0)
       vdp_cmd_execute_LMMV(0, i,     256, hh, c, VDP_CMD_OR);
@@ -154,10 +191,17 @@ static void init_variables(void) {
   R = r = 0;
   x_pos = x_pos_d = vx = 0;
 
-  for (uint8_t n = RASTER_ROWS; n--; ) {
-    scroll_x[0][n] = CENTER;
-    scroll_x[1][n] = CENTER;
+  idx = 0;
+  const uint8_t r26 = hscroll_register_r26_value_from(CENTER); // R#26
+  const uint8_t r27 = hscroll_register_r27_value_from(CENTER); // R#27
+  uint8_t * p = (uint8_t *)scroll_table;
+  for (uint8_t n = 2 * RASTER_ROWS; n--; ) {
+    *p++ = r26;
+    *p++ = r27;
   }
+
+  memset(curvatures, 0, sizeof(curvatures));
+  curvatures_idx = 0;
 }
 
 static void road_animation(void) {
@@ -176,34 +220,73 @@ static void road_animation(void) {
 }
 
 static void update_road_geometry(void) {
-  // generate curve
-  if (!(JIFFY & 127)) {
-    if (R) {
+  // generate curve every 64 ticks
+  if (!(JIFFY & 63)) {
+    uint16_t a = rand();
+    if (!(a & 3)) {
+      // return to straight course in 25% probability.
       R = 0;
-      // hide curve indicator
       hide_curve_indicator();
     }
     else {
-      uint16_t a = rand() & 255;
+      a = (a >> 2) & 255;
       if (a < 2 * MAX_R + 1) {
+        // change target curvature `R` to random value in range of -MAX_R to +MAX_R
         R = a - MAX_R;
-        if (MAX_R / 4 < R) {
+        // show curve sign before reach to the curve if it is sharp curve.
+        if (MAX_R / 8 < R) {
           show_curve_indicator_right();
-        } else if (R < -MAX_R / 4) {
+        }
+        else if (R < -MAX_R / 8) {
           show_curve_indicator_left();
+        }
+        else if (-MAX_R / 16 < R && R < MAX_R / 16) {
+          hide_curve_indicator();
         }
       }
       else {
-        R = 0;
+        // keep target curvature `R`
       }
     }
   }
+  // Gradually bring the curvature closer to the target value.
   if (r < R) {
     r++;
   }
   if (R < r) {
     r--;
   }
+  // update the ring buffer (history) of curvature
+  curvatures[curvatures_idx++] = r;
+  if (sizeof(curvatures) <= curvatures_idx) {
+    curvatures_idx = 0;
+  }
+  r0 = curvatures[curvatures_idx];
+}
+
+inline void update_raster_scroll_table(void) {
+  const uint8_t k = idx ^ 1;
+  uint16_t x = (RASTER_ROWS - 1) * x_pos;
+  uint16_t dx = 0;
+  const int8_t * p_curvatures = &curvatures[curvatures_idx];
+  uint8_t j = curvatures_idx;
+  uint8_t * p = ((uint8_t *)&scroll_table[k][RASTER_ROWS]) - 1;
+  for (uint8_t i = RASTER_ROWS; i--; ) {
+    // const uint16_t sx = CENTER + (x >> 7);
+    // *p-- = hscroll_register_r27_value_from(sx); // R#27
+    // *p-- = hscroll_register_r26_value_from(sx); // R#26
+    const uint16_t sx = CENTER + (x >> 7) + 7;
+    *p-- = ~((uint8_t)sx) & 7;  // R#27
+    *p-- = (sx >> 3) & 0x3f;    // R#26
+    dx -= *p_curvatures++;
+    x += dx - x_pos;
+    j++;
+    if (sizeof(curvatures) <= j) {
+      j = 0;
+      p_curvatures = &curvatures[0];
+    }
+  }
+  idx ^= 1;
 }
 
 void main(void) {
@@ -226,6 +309,7 @@ void main(void) {
 
   init_variables();
 
+  show_vehicle();
   // main loop of the game
   for (;;) {
     await_vsync();
@@ -233,35 +317,47 @@ void main(void) {
     update_road_geometry();     // update curvature of the road.
 
     // update scroll amount for distant landscape
-    bg_scroll_x_d -= r;
+    bg_scroll_x_d -= r0;
     bg_scroll_x -= bg_scroll_x_d / 16;
     bg_scroll_x_d %= 16;
 
     // steering
     uint8_t joy = joypad_get_state(0);
     if (joy & VK_RIGHT) {
+      if (56 <= vx && vx < 64) {
+        show_vehicle_R();
+      }
       vx += 8;
     }
     else if (joy & VK_LEFT) {
+      if (-64 < vx && vx <= -56) {
+        show_vehicle_L();
+      }
       vx -= 8;
     }
     else {
       if (0 < vx) {
+        if (28 <= vx && vx < 32) {
+          show_vehicle();
+        }
         vx -= 4;
       }
       else if (vx < 0) {
+        if (-32 < vx && vx <= -28) {
+          show_vehicle();
+        }
         vx += 4;
       }
     }
-    if (64 < vx) {
-      vx = 64;
+    if (96 < vx) {
+      vx = 96;
     }
-    else if (vx < -64) {
-      vx = -64;
+    else if (vx < -96) {
+      vx = -96;
     }
     // slide vehicle's lateral position according to steering and road curvature.
     x_pos_d += vx;
-    x_pos_d -= 2 * r;
+    x_pos_d -= 2 * r0;
     x_pos += x_pos_d / 8;
     x_pos_d %= 8;
 
@@ -281,16 +377,6 @@ void main(void) {
     }
 
     // update scroll amount for each raster
-    const uint8_t k = idx ^ 1;
-    uint16_t x = 0;
-    uint16_t px = (RASTER_ROWS - 1) * x_pos;
-    uint16_t dx = 0;
-    for (uint8_t i = RASTER_ROWS; i--; ) {
-      scroll_x[k][i] = (CENTER + ((x + px) >> 7)) & 511;
-      x += dx;
-      dx -= r;
-      px -= x_pos;
-    }
-    idx ^= 1;
+    update_raster_scroll_table();
   }
 }
--- sample/driving/src/sprites.c
+++ sample/driving/src/sprites.c
@@ -13,14 +13,23 @@
  * \brief SPRITE part of `MSX2+` driving game demo.
  */
 
-#include <msx.h>
-
 #include "./sprites.h"
 
+#include <msx.h>
+
+#include <stdint.h>
+#include <string.h>
+
+#define SPRITE_COLORS   (SPRITES - 512) // sprite color table
 #define SPRITES         (0x07600) // sprite attribute table
 #define SPRITE_PATTERNS (0x07800) // sprite pattern generator table
 
-const uint8_t sprites[][32] = {
+static struct sprite_table {
+  struct sprite_color color[32]; // sprite color table
+  struct sprite attr[32];        // sprite attribute table
+} sprites;
+
+const uint8_t sprite_patterns[][32] = {
   [0] = {                       /* << */
     0b01111111,
     0b10000000,
@@ -132,31 +141,350 @@ const uint8_t sprites[][32] = {
     0b11111110,
     0b00000000,
   },
+
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+  // ________ _******* *******_ ________
+  // _______* ******** ******** *_______
+  // ______** ******** ******** **______
+  // ______** ******** ******** **______
+  // _____*** ******** ******** ***_____
+  // _____*** ******** ******** ***_____
+  // ____**** ******** ******** ****____
+  // ____**__ ________ ________ __**____
+
+  // ___***** ******** ******** *****___
+  // ___***** ******** ******** *****___
+  // __****** ******** ******** ******__
+  // __****** ******** ******** ******__
+  // _******* ******** ******** *******_
+  // _******* ******** ******** *******_
+  // _******* ******** ******** *******_
+  // _******* ******** ******** *******_
+  // _******* ******** ******** *******_
+  // __****** ******** ******** ******__
+  // __****** ******** ******** ******__
+  // ___***** ******** ******** *****___
+  // ____**** ***_____ _____*** ****____
+  // ____**** ***_____ _____*** ****____
+  // ________ ________ ________ ________
+  // ________ ________ ________ ________
+
+  [3] = {                       /* vehicle(0,0) */
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000001,
+    0b00000011,
+    0b00000011,
+    0b00000111,
+    0b00000111,
+    0b00001111,
+    0b00001100,
+
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b01111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b00000000,
+  },
+
+  [4] = {                       /* vehicle(1,0) */
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b11111110,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b00000000,
+
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b10000000,
+    0b11000000,
+    0b11000000,
+    0b11100000,
+    0b11100000,
+    0b11110000,
+    0b00110000,
+  },
+
+  [5] = {                       /* vehicle(0,1) */
+    0b00011111,
+    0b00011111,
+    0b00111111,
+    0b00111111,
+    0b01111111,
+    0b01111111,
+    0b01111111,
+    0b01111111,
+    0b01111111,
+    0b00111111,
+    0b00111111,
+    0b00011111,
+    0b00001111,
+    0b00001111,
+    0b00000000,
+    0b00000000,
+
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11100000,
+    0b11100000,
+    0b00000000,
+    0b00000000,
+  },
+
+  [6] = {                       /* vehicle(1,1) */
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b00000111,
+    0b00000111,
+    0b00000000,
+    0b00000000,
+
+    0b11111000,
+    0b11111000,
+    0b11111100,
+    0b11111100,
+    0b11111110,
+    0b11111110,
+    0b11111110,
+    0b11111110,
+    0b11111110,
+    0b11111100,
+    0b11111100,
+    0b11111000,
+    0b11110000,
+    0b11110000,
+    0b00000000,
+    0b00000000,
+  },
 };
 
-const vec2i_t layouts[] = { { 0, 0, }, { 0, 0, }, };
+const tagged_color_t curve_indicators_colors[] = { 8, 10, };
+const vec2i_t curve_indicator_layouts[] = { {0}, {0}, };
 const uint8_t curve_indicator_L_pats[] = { 0, 8, };
 const uint8_t curve_indicator_R_pats[] = { 4, 8, };
 const metasprite_t curve_indicators[] = {
-  [0] = { .n = 2, .layouts = (vec2i_t *)layouts, .pats = (uint8_t *)&curve_indicator_L_pats[0], },
-  [1] = { .n = 2, .layouts = (vec2i_t *)layouts, .pats = (uint8_t *)&curve_indicator_R_pats[0], },
+  [0] = { .n = 2, .layouts = (vec2i_t *)curve_indicator_layouts, .pats = (uint8_t *)&curve_indicator_L_pats[0], },
+  [1] = { .n = 2, .layouts = (vec2i_t *)curve_indicator_layouts, .pats = (uint8_t *)&curve_indicator_R_pats[0], },
 };
-const tagged_color_t curve_indicators_color[] = { 8, 10, };
+
+const tagged_color_t vehicle_colors[] = { 1, 1, 1, 1 };
+const vec2i_t vehicle_layouts[] = {
+  { 0, 0}, {32, 0},
+  { 0,32}, {32,32},
+};
+const vec2i_t vehicle_L_layouts[] = {
+  {-2, 0}, {30,-1},
+  { 0,32}, {32,31},
+};
+const vec2i_t vehicle_R_layouts[] = {
+  { 2,-1}, {34, 0},
+  { 0,31}, {32,32},
+};
+const uint8_t vehicle_pats[] = {
+  12, 16,
+  20, 24,
+};
+const metasprite_t vehicle = {
+  .n = 4, .layouts = (vec2i_t *)vehicle_layouts, .pats = (uint8_t *)vehicle_pats,
+};
+const metasprite_t vehicle_L = {
+  .n = 4, .layouts = (vec2i_t *)vehicle_L_layouts, .pats = (uint8_t *)vehicle_pats,
+};
+const metasprite_t vehicle_R = {
+  .n = 4, .layouts = (vec2i_t *)vehicle_R_layouts, .pats = (uint8_t *)vehicle_pats,
+};
+
+void update_sprite_colors(void) {
+  vmem_write(SPRITE_COLORS, (void *)&sprites.color, sizeof(sprites.color));
+}
+
+void update_sprite_attributes(void) {
+  vmem_write(SPRITES, (void *)&sprites.attr, sizeof(sprites.attr));
+}
+
+void update_sprite_color(uint8_t plane) {
+  // assert(plane < 32);
+  vmem_write(SPRITE_COLORS + sizeof(struct sprite_color) * plane,
+             (void *)&sprites.color[plane],
+             sizeof(struct sprite_color));
+}
+
+void update_sprite_attribute(uint8_t plane) {
+  // assert(plane < 32);
+  vmem_write(SPRITES + sizeof(struct sprite) * plane,
+             (void *)&sprites.attr[plane],
+             sizeof(struct sprite));
+}
+
+static void set_sprite_color(uint8_t plane, tagged_color_t color) {
+  // assert(plane < 32);
+  memset(&sprites.color[plane], color, sizeof(struct sprite_color));
+  update_sprite_color(plane);
+}
+
+static void set_sprite_colors(uint8_t plane, uint8_t n, const tagged_color_t * colors) {
+  // assert(plane < 32 && plane + n <= 32);
+  while (n--) {
+    set_sprite_color(plane++, *colors++);
+  }
+}
+
+static void set_sprite_raster_color(uint8_t plane, const struct sprite_color * color) {
+  // assert(plane < 32);
+  memcpy(&sprites.color[plane], color, sizeof(struct sprite_color));
+  update_sprite_color(plane);
+}
+
+static void set_sprite_raster_colors(uint8_t plane, uint8_t n, const struct sprite_color * colors) {
+  // assert(plane < 32 && plane + n <= 32);
+  while (n--) {
+    set_sprite_raster_color(plane++, colors++);
+  }
+}
+
+static void set_metasprite(uint8_t plane, const metasprite_t * ms, int x, int y) {
+  // assert(plane < 32 && plane + ms->n <= 32);
+  struct sprite * sp = &sprites.attr[plane];
+  const uint8_t n = ms->n;
+  const int dx = x - ms->anchor.x;
+  const int dy = y - ms->anchor.y;
+  for (uint8_t i = 0; i < n; ++i) {
+    sp->pat = ms->pats[i];
+    sprite_set_x(sp, ms->layouts[i].x + dx);
+    sprite_set_y(sp, ms->layouts[i].y + dy);
+    if (sp->tagged_color & SPRITE_TAG_EC) {
+      // if there is a sprite whose x-coordinate would to be less than 0 on the
+      // screen, that sprite is hidden instead of setting the EC bit.
+      sp->y = 217;
+    }
+    sp++;
+  }
+}
+
+static void unset_metasprite(uint8_t plane, const metasprite_t * ms) {
+  // assert(plane < 32 && plane + ms->n <= 32);
+  struct sprite * sp = &sprites.attr[plane];
+  for (uint8_t i = ms->n; i--; ) {
+    sp->y = 217;
+    sp++;
+  }
+}
+
+static void show_metasprite(uint8_t plane, const metasprite_t * ms, int x, int y) {
+  // assert(plane < 32 && plane + ms->n <= 32);
+  set_metasprite(plane, ms, x, y);
+  vmem_write(SPRITES + sizeof(struct sprite) * plane,
+             &sprites.attr[plane],
+             sizeof(struct sprite) * ms->n);
+}
+
+static void hide_metasprite(uint8_t plane, const metasprite_t * ms) {
+  // assert(plane < 32 && plane + ms->n <= 32);
+  unset_metasprite(plane, ms);
+  vmem_write(SPRITES + sizeof(struct sprite) * plane,
+             &sprites.attr[plane],
+             sizeof(struct sprite) * ms->n);
+}
 
 void setup_sprites(void) {
   vdp_set_sprite_size(VDP_SPRITE_SIZE_16x16_MAGNIFIED);
-  vmem_write(SPRITE_PATTERNS, (void *)sprites, sizeof(sprites));
-  vmem_set_sprite_color_m(SPRITES, 0, 2, curve_indicators_color);
+  vmem_write(SPRITE_PATTERNS, (void *)sprite_patterns, sizeof(sprite_patterns));
+  // vmem_set_sprite_color_m(SPRITES, 0, 2, curve_indicators_colors);
+  set_sprite_colors(0, 2, curve_indicators_colors);
+  // vmem_set_sprite_color_m(SPRITES, 2, 4, vehicle_colors);
+  set_sprite_colors(2, 4, vehicle_colors);
 }
 
 void hide_curve_indicator(void) {
-  vmem_set_metasprite_a(SPRITES, 0, 0, 217, &curve_indicators[0]);
+  // vmem_set_metasprite_a(SPRITES, 0, 0, 217, &curve_indicators[0]);
+  hide_metasprite(0, &curve_indicators[0]);
 }
 
 void show_curve_indicator_right(void) {
-  vmem_set_metasprite_a(SPRITES, 0, 160, 64, &curve_indicators[1]);
+  // vmem_set_metasprite_a(SPRITES, 0, 160, 64, &curve_indicators[1]);
+  show_metasprite(0, &curve_indicators[1], 160, 64);
 }
 
 void show_curve_indicator_left(void) {
-  vmem_set_metasprite_a(SPRITES, 0, 64, 64, &curve_indicators[0]);
+  // vmem_set_metasprite_a(SPRITES, 0, 64, 64, &curve_indicators[0]);
+  show_metasprite(0, &curve_indicators[0], 64, 64);
+}
+
+void hide_vehicle(void) {
+  hide_metasprite(2, &vehicle);
+}
+
+void show_vehicle(void) {
+  show_metasprite(2, &vehicle, 96, 140);
+}
+
+void show_vehicle_L(void) {
+  show_metasprite(2, &vehicle_L, 96, 140);
+}
+
+void show_vehicle_R(void) {
+  show_metasprite(2, &vehicle_R, 96, 140);
 }

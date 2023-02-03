9f67e170c441ad3366cdb34bb142d9b0a26b0015
mori0091 <mori-d@qc4.so-net.ne.jp>
2023-02-04 08:15:17 +0900

    refactor(snddrv): improved `snd_m`

--- src/snd_m.c
+++ src/snd_m.c
@@ -63,16 +63,17 @@ static void snd_m__set_Pattern(struct snd_m_ctx * ctx, const snd_Music * pg, con
   }
 }
 
-static uint8_t snd_m__stream_take(struct snd_m_ctx * ctx);
-static void snd_m__decode_channel(struct snd_m_ctx * ctx, struct snd_channel * pch);
-static void snd_m__decode_expression_command(struct snd_m_ctx * ctx, struct snd_channel * pch);
+void snd_m__decode_channel(struct snd_channel * pch);
+void snd_m__decode_expression_command(struct snd_channel * pch);
 
 inline void snd_m__update(struct snd_m_ctx * ctx) {
   struct snd_channel * pch = ctx->channels;
-  for (uint8_t ch = 3; ch--; pch++) {
-    snd_channel_update(pch);
-  }
+  snd_channel_update(pch++);
+  snd_channel_update(pch++);
+  snd_channel_update(pch);
 }
+
+static struct snd_m_ctx * m_ctx;
 
 void snd_m__decode(struct snd_m_ctx * ctx) {
   snd_m__update(ctx);
@@ -82,37 +83,36 @@ void snd_m__decode(struct snd_m_ctx * ctx) {
     return;
   }
   // ---------------------------------------------------
-  const snd_Music * pg = ctx->music;
-  const snd_Pattern * p = &pg->patterns.data[ctx->pindex];
-  if (p->height <= ctx->line) {
-    // Proceed to the next pattern
-    ctx->line = 0;
-    ctx->pindex++;
-    p++;
-    if (pg->patterns.length <= ctx->pindex) {
-      // End of the list of patterns
-      if (pg->isLoop && pg->loopToIndex < pg->patterns.length) {
+  {
+    const snd_Music * pg = ctx->music;
+    const snd_Pattern * p = &pg->patterns.data[ctx->pindex];
+    if (p->height <= ctx->line) {
+      // Proceed to the next pattern
+      ctx->line = 0;
+      ctx->pindex++;
+      p++;
+      if (pg->patterns.length <= ctx->pindex) {
+        // End of the list of patterns
+        if (!pg->isLoop) {
+          // finish the program (unless 'auto-repeat' is set)
+          ctx->isEnd = true;
+          return;
+        }
         // Loop back to the specified pattern
+        // assert(pg->loopToIndex < pg->patterns.length);
         ctx->pindex = pg->loopToIndex;
         p = &pg->patterns.data[ctx->pindex];
       }
-      else {
-        // finish the program (unless 'auto-repeat' is set)
-        ctx->isEnd = true;
-        return;
-      }
+      snd_m__set_Pattern(ctx, pg, p);
     }
-    snd_m__set_Pattern(ctx, pg, p);
   }
-  for (uint8_t ch = 3; ch--; ) {
-    struct snd_channel * pch = &ctx->channels[ch];
-    struct snd_t_ctx * tctx = &pch->t;
-    if (tctx->skip_count) {
-      tctx->skip_count--;
-    }
-    else {
-      snd_m__decode_channel(ctx, pch);
-    }
+  // ----
+  {
+    m_ctx = ctx;
+    struct snd_channel * pch = &ctx->channels[0];
+    snd_m__decode_channel(pch++);
+    snd_m__decode_channel(pch++);
+    snd_m__decode_channel(pch);
   }
   // ----
   if (ctx->spd.next) {
@@ -146,11 +146,14 @@ void snd_m__decode(struct snd_m_ctx * ctx) {
   ctx->line++;
 }
 
-static void snd_m__decode_channel(struct snd_m_ctx * ctx, struct snd_channel * pch) {
-  (void)ctx;
+void snd_m__decode_channel(struct snd_channel * pch) {
+  if (pch->t.skip_count) {
+    pch->t.skip_count--;
+    return;
+  }
   // t_chunk
   for (;;) {
-    uint8_t x = snd_t_stream_take(&pch->t);
+    const uint8_t x = snd_t_stream_take(&pch->t);
     // note command
     if (!x) {                   // x == 00000000b
       // NoteOff
@@ -160,8 +163,9 @@ static void snd_m__decode_channel(struct snd_m_ctx * ctx, struct snd_channel * p
       // NoteOn i# (or Legato)
       const uint8_t i_number = snd_t_stream_take(&pch->t);
       const snd_Instrument * inst = 0;
-      if (i_number && i_number <= ctx->sa->instruments.length) {
-        inst = &ctx->sa->instruments.data[i_number - 1];
+      if (i_number) {
+        // assert(i_number <= m_ctx->sa->instruments.length);
+        inst = &m_ctx->sa->instruments.data[i_number - 1];
       }
       snd_channel_note_on(x, inst, pch);
     }
@@ -169,7 +173,7 @@ static void snd_m__decode_channel(struct snd_m_ctx * ctx, struct snd_channel * p
       // expressions
       uint8_t n = x & 0x0f;
       do {
-        snd_m__decode_expression_command(ctx, pch);
+        snd_m__decode_expression_command(pch);
       } while (n--);
     }
     else if (!(x & 0x20)) {     // x == 110*****b
@@ -187,89 +191,130 @@ static void snd_m__decode_channel(struct snd_m_ctx * ctx, struct snd_channel * p
   }
 }
 
-static void snd_m__decode_expression_command(struct snd_m_ctx * ctx, struct snd_channel * pch) {
+typedef void (* expression_command_func)(uint8_t x, struct snd_channel * pch);
+
+static void expression_command_0(uint8_t x, struct snd_channel * pch) {
+  snd_channel_reset_expression(pch);
+  pch->volume = 15 - x;
+}
+static void expression_command_1(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // arpeggio 3 notes
+  // set arp to (+0, +x, +y);
+  snd_channel_set_arpeggio(0, 3, xyz, pch);
+}
+static void expression_command_2(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // arpeggio 4 notes
+  // set arp to (+0, +x, +y, +z);
+  snd_channel_set_arpeggio(0, 4, xyz, pch);
+}
+static void expression_command_3(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // pitch up (+0..+4095/128)
+  // snd_channel_set_pitch_bend(5, xyz, pch);
+  snd_channel_set_period_bend(-1, xyz >> 4, pch);
+}
+static void expression_command_4(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // pitch down (-4095/128..+0)
+  // snd_channel_set_pitch_bend(5, -xyz, pch);
+  snd_channel_set_period_bend(+1, xyz >> 4, pch);
+}
+static void expression_command_5(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // fast pitch up (+0..+4095/128)
+  // snd_channel_set_pitch_bend(0, xyz, pch);
+  snd_channel_set_period_bend(-1, xyz, pch);
+}
+static void expression_command_6(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // fast pitch down (-4095/128..+0)
+  // snd_channel_set_pitch_bend(0, -xyz, pch);
+  snd_channel_set_period_bend(+1, xyz, pch);
+}
+static void expression_command_7(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // pitch glide
+  pch->pitch_glide = xyz;
+}
+static void expression_command_8(uint8_t x, struct snd_channel * pch) {
+  // set volume to x
+  pch->volume = x;
+  pch->fade_speed = 0;
+}
+static void expression_command_9(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  snd_channel_set_fade(+1, xyz, pch);
+}
+static void expression_command_10(uint8_t x, struct snd_channel * pch) {
+  const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  snd_channel_set_fade(-1, xyz, pch);
+}
+static void expression_command_11(uint8_t x, struct snd_channel * pch) {
+  // const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // const uint8_t xy = xyz >> 4;
+  const uint8_t xy = (x << 4) + (snd_t_stream_take(&pch->t) >> 4);
+  // force the speed of instrument
+  pch->i.wait = xy;
+}
+static void expression_command_12(uint8_t x, struct snd_channel * pch) {
+  // const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // const uint8_t xy = xyz >> 4;
+  const uint8_t xy = (x << 4) + (snd_t_stream_take(&pch->t) >> 4);
+  // force the speed of arpeggio
+  pch->a.wait = xy;
+}
+static void expression_command_13(uint8_t x, struct snd_channel * pch) {
+  // const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // const uint8_t xy = xyz >> 4;
+  const uint8_t xy = (x << 4) + (snd_t_stream_take(&pch->t) >> 4);
+  // force the speed of pitch
+  pch->p.wait = pch->pitch_wait = xy;
+}
+static void expression_command_14(uint8_t x, struct snd_channel * pch) {
+  // const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // const uint8_t idx = xyz >> 4;
+  const uint8_t idx = (x << 4) + (snd_t_stream_take(&pch->t) >> 4);
+  const snd_PitchBend * p = 0;
+  if (idx && idx <= m_ctx->sa->pitchBendTables.length) {
+    p = &m_ctx->sa->pitchBendTables.data[idx - 1];
+  }
+  snd_a__program_change(&pch->a, p);
+}
+static void expression_command_15(uint8_t x, struct snd_channel * pch) {
+  // const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
+  // const uint8_t idx = xyz >> 4;
+  const uint8_t idx = (x << 4) + (snd_t_stream_take(&pch->t) >> 4);
+  const snd_PeriodBend * p = 0;
+  if (idx && idx <= m_ctx->sa->periodBendTables.length) {
+    p = &m_ctx->sa->periodBendTables.data[idx - 1];
+  }
+  snd_p__program_change(&pch->p, p);
+}
+
+static const expression_command_func expression_commands[] = {
+  expression_command_0,
+  expression_command_1,
+  expression_command_2,
+  expression_command_3,
+  expression_command_4,
+  expression_command_5,
+  expression_command_6,
+  expression_command_7,
+  expression_command_8,
+  expression_command_9,
+  expression_command_10,
+  expression_command_11,
+  expression_command_12,
+  expression_command_13,
+  expression_command_14,
+  expression_command_15,
+};
+
+void snd_m__decode_expression_command(struct snd_channel * pch) {
   // decode an expression command
-  uint8_t x = snd_t_stream_take(&pch->t);
+  const uint8_t x = snd_t_stream_take(&pch->t);
   const uint8_t tag = x >> 4;
-  x &= 15;
-  if (!tag) {
-    snd_channel_reset_expression(pch);
-    pch->volume = 15 - x;
-  }
-  else if (tag == 8) {
-    // set volume to x
-    pch->volume = x;
-    pch->fade_speed = 0;
-  }
-  else {
-    const uint16_t xyz = (x << 8) + snd_t_stream_take(&pch->t);
-    if (tag == 1) {
-      // arpeggio 3 notes
-      // set arp to (+0, +x, +y);
-      snd_channel_set_arpeggio(0, 3, xyz, pch);
-    }
-    else if (tag == 2) {
-      // arpeggio 4 notes
-      // set arp to (+0, +x, +y, +z);
-      snd_channel_set_arpeggio(0, 4, xyz, pch);
-    }
-    else if (tag == 3) {
-      // // pitch up (+0..+4095/128)
-      // snd_channel_set_pitch_bend(5, xyz, pch);
-      snd_channel_set_period_bend(-1, xyz >> 4, pch);
-    }
-    else if (tag == 4) {
-      // // pitch down (-4095/128..+0)
-      // snd_channel_set_pitch_bend(5, -xyz, pch);
-      snd_channel_set_period_bend(+1, xyz >> 4, pch);
-    }
-    else if (tag == 5) {
-      // // fast pitch up (+0..+4095/128)
-      // snd_channel_set_pitch_bend(0, xyz, pch);
-      snd_channel_set_period_bend(-1, xyz, pch);
-    }
-    else if (tag == 6) {
-      // // fast pitch down (-4095/128..+0)
-      // snd_channel_set_pitch_bend(0, -xyz, pch);
-      snd_channel_set_period_bend(+1, xyz, pch);
-    }
-    else if (tag == 7) {
-      // pitch glide
-      pch->pitch_glide = xyz;
-    }
-    else if (tag == 9) {
-      snd_channel_set_fade(+1, xyz, pch);
-    }
-    else if (tag == 10) {
-      snd_channel_set_fade(-1, xyz, pch);
-    }
-    else if (tag == 11) {
-      // force the speed of instrument
-      pch->i.wait = xyz >> 4;
-    }
-    else if (tag == 12) {
-      // force the speed of arpeggio
-      pch->a.wait = xyz >> 4;
-    }
-    else if (tag == 13) {
-      // force the speed of pitch
-      pch->p.wait = pch->pitch_wait = xyz >> 4;
-    }
-    else if (tag == 14) {
-      uint8_t idx = xyz >> 4;
-      const snd_PitchBend * p = 0;
-      if (idx && idx <= ctx->sa->pitchBendTables.length) {
-        p = &ctx->sa->pitchBendTables.data[idx - 1];
-      }
-      snd_a__program_change(&pch->a, p);
-    }
-    else if (tag == 15) {
-      uint8_t idx = xyz >> 4;
-      const snd_PeriodBend * p = 0;
-      if (idx && idx <= ctx->sa->periodBendTables.length) {
-        p = &ctx->sa->periodBendTables.data[idx - 1];
-      }
-      snd_p__program_change(&pch->p, p);
-    }
-  }
+  expression_commands[tag](x & 15, pch);
 }

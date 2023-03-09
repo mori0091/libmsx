d7cb374fe96cf1c741b6d4a0e7f3b94f2470efb9
mori0091 <mori-d@qc4.so-net.ne.jp>
2023-03-10 01:06:57 +0900

    fix(vmem): Improved `vmem_write()`.
    Reimplemented `vmem_write()` to use `vmem_write_chunk()`.

--- src/vmem_write.c
+++ src/vmem_write.c
@@ -1,23 +1,24 @@
 // -*- coding: utf-8-unix -*-
-/**
- * \file vmem_write.c
- *
+/*
  * Copyright (c) 2021-2023 Daishi Mori (mori0091)
  *
- * This software is released under the MIT License.
+ * This software is released under the MIT License.\n
  * See https://github.com/mori0091/libmsx/blob/main/LICENSE
  *
- * GitHub libmsx project
+ * GitHub libmsx project\n
  * https://github.com/mori0091/libmsx
+ */
+/**
+ * \file vmem_write.c
  */
 
 #include "../include/vdp.h"
 
 #include "vdp_internal.h"
 
+void vmem_write_chunk(const uint8_t * p, uint16_t n);
+
 void vmem_write(vmemptr_t dst, void* src, uint16_t len) {
   vmem_set_write_address(dst);
-  for (uint8_t* p = src; len--; ) {
-    VDP_SET_VMEM_VALUE(*p++);
-  }
+  vmem_write_chunk((const uint8_t *)src, len);
 }

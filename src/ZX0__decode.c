d69265abca7bb8efa6c320af9707ed45717bc375
mori0091 <mori-d@qc4.so-net.ne.jp>
2023-03-13 16:07:35 +0900

    refactor(ZX0): refactored for modular design.

---
+++ src/ZX0__decode.c
@@ -0,0 +1,113 @@
+// -*- coding: utf-8-unix -*-
+/*
+ * Copyright (c) 2021-2023 Daishi Mori (mori0091)
+ *
+ * This software is released under the MIT License.\n
+ * See https://github.com/mori0091/libmsx/blob/main/LICENSE
+ *
+ * GitHub libmsx project\n
+ * https://github.com/mori0091/libmsx
+ */
+/**
+ * \file ZX0__decode.c
+ *
+ * Decompresser of ZX0 compressed file format version 2.
+ *
+ * The ZX0 data compression format and algorithm was designed by Einar Saukas.
+ *
+ * \sa https://github.com/einar-saukas/ZX0
+ *
+ * This C implementation of the ZX0 decompressor has been reimplemented for use
+ * with libmsx by Daishi Mori (mori0091). It provides only the core part of the
+ * ZX0 decompressor, which is independent of input/output memory and storage
+ * type.
+ */
+
+#include "ZX0__decode.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+static uint8_t bit_mask;
+static uint8_t bit_value;
+
+void (* ZX0__fnptr_duplicate)(uint16_t offset, size_t len);
+void (* ZX0__fnptr_transfer)(size_t len);
+uint8_t (* ZX0__fnptr_read_byte)(void);
+
+static inline void duplicate_bytes(uint16_t offset, size_t len) {
+  ZX0__fnptr_duplicate(offset, len);
+}
+
+static inline void transfer_bytes(size_t len) {
+  ZX0__fnptr_transfer(len);
+}
+
+static inline uint8_t read_byte(void) {
+  return ZX0__fnptr_read_byte();
+}
+
+static bool read_bit(void) {
+  bit_mask >>= 1;
+  if (!bit_mask) {
+    bit_mask = 0x80;
+    bit_value = read_byte();
+  }
+  return !!(bit_value & bit_mask);
+}
+
+static uint16_t read_interlaced_elias_gamma(void) {
+  uint16_t value = 1;
+  while (!read_bit()) {
+    value = (value << 1) | read_bit();
+  }
+  return value;
+}
+
+static uint16_t read_interlaced_elias_gamma_with_backtrack(uint8_t last_byte) {
+  if (last_byte & 1) {
+    return 2;
+  }
+  uint16_t value = 1;
+  do {
+    value = (value << 1) | read_bit();
+  } while (!read_bit());
+  return value + 1;
+}
+
+static uint16_t read_interlaced_elias_gamma_inverted(void) {
+  uint16_t value = 1;
+  while (!read_bit()) {
+    value = (value << 1) | !read_bit();
+  }
+  return value;
+}
+
+void ZX0__decode(void) {
+  uint16_t offset = 1;
+  bit_mask = 0;
+  for (;;) {
+    do {
+      // COPY LITERALS
+      transfer_bytes(read_interlaced_elias_gamma());
+      if (read_bit()) {
+        break;
+      }
+      // COPY FROM THE LAST OFFSET
+      duplicate_bytes(offset, read_interlaced_elias_gamma());
+    } while (!read_bit());
+
+    do {
+      // COPY FROM NEW OFFSET
+      uint16_t offset_hi = read_interlaced_elias_gamma_inverted();
+      if (offset_hi == 256) {
+        return;
+      }
+      uint8_t offset_lo = read_byte();
+      offset = offset_hi * 128 - (offset_lo >> 1);
+      duplicate_bytes(offset, read_interlaced_elias_gamma_with_backtrack(offset_lo));
+    } while (read_bit());
+  }
+}
--- src/zx0_decompress.c
+++ src/zx0_decompress.c
@@ -9,120 +9,33 @@
  * https://github.com/mori0091/libmsx
  */
 /**
- * \file zx0_decompress.c
+ * \file ZX0_decompress.c
  */
 
-// #include "zx0_decompress.h"
+#include "ZX0_decompress.h"
+#include "xmem.h"
 
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <string.h>
+#include "./ZX0__decode.h"
 
 static const uint8_t * src_ptr;
-static uint8_t * dst_ptr;
-static uint16_t last_offset;
-static uint8_t bit_mask;
-static uint8_t bit_value;
 
-#if defined(__SDCC)
-
-static void copy_from(const uint8_t * src, size_t len) {
-  memcpy(dst_ptr, src, len);
-  dst_ptr += len;
-}
-
-#else
-
-static void copy_from(const uint8_t * src, size_t len) {
-  // ** cannot use memcpy()/memmove() in this case. **
-  // - copying by memcpy() of overlapping area is undefined behavior.
-  // - memmove() does not causes side effect (repitition), expected in LZ77/LZSS
-  //   algorithm.
-  // memcpy(dst_ptr, src, len);
-  // dst_ptr += len;
-  while (len--) {
-    *dst_ptr++ = *src++;
-  }
-}
-
-#endif
-
-static void duplicate_bytes(size_t len) {
-  copy_from(dst_ptr - last_offset, len);
-}
-
-static void transfer_bytes(size_t len) {
-  copy_from(src_ptr, len);
+static void default_transfer(size_t len) {
+  mem_write_chunk(src_ptr, len);
   src_ptr += len;
 }
 
-static uint8_t read_byte(void) {
+static uint8_t default_read_byte(void) {
   return *src_ptr++;
 }
 
-static bool read_bit(void) {
-  bit_mask >>= 1;
-  if (!bit_mask) {
-    bit_mask = 0x80;
-    bit_value = read_byte();
-  }
-  return !!(bit_value & bit_mask);
-}
-
-static uint16_t read_interlaced_elias_gamma(void) {
-  uint16_t value = 1;
-  while (!read_bit()) {
-    value = (value << 1) | read_bit();
-  }
-  return value;
-}
-
-static uint16_t read_interlaced_elias_gamma_with_backtrack(uint8_t last_byte) {
-  if (last_byte & 1) {
-    return 2;
-  }
-  uint16_t value = 1;
-  do {
-    value = (value << 1) | read_bit();
-  } while (!read_bit());
-  return value + 1;
-}
-
-static uint16_t read_interlaced_elias_gamma_inverted(void) {
-  uint16_t value = 1;
-  while (!read_bit()) {
-    value = (value << 1) | !read_bit();
-  }
-  return value;
-}
-
-size_t zx0_decompress(const uint8_t * src, uint8_t * dst) {
+size_t ZX0_decompress(const uint8_t * src, uint8_t * dst) {
+  ZX0__fnptr_duplicate = mem_dup;
+  ZX0__fnptr_transfer  = default_transfer;
+  ZX0__fnptr_read_byte = default_read_byte;
   src_ptr = src;
-  dst_ptr = dst;
-  last_offset = 1;
-  bit_mask = 0;
-
-  for (;;) {
-    do {
-      // COPY_LITERALS:
-      transfer_bytes(read_interlaced_elias_gamma());
-      if (read_bit()) {
-        break;
-      }
-      // COPY_FROM_LAST_OFFSET:
-      duplicate_bytes(read_interlaced_elias_gamma());
-    } while (!read_bit());
-
-    do {
-      // COPY_FROM_NEW_OFFSET:
-      uint16_t offset_hi = read_interlaced_elias_gamma_inverted();
-      if (offset_hi == 256) {
-        return dst_ptr - dst;
-      }
-      uint8_t offset_lo = read_byte();
-      last_offset = offset_hi * 128 - (offset_lo >> 1);
-      duplicate_bytes(read_interlaced_elias_gamma_with_backtrack(offset_lo));
-    } while (read_bit());
-  }
+  mem_open(dst);
+  ZX0__decode();
+  size_t s = mem_ptr() - dst;
+  mem_close();
+  return s;
 }

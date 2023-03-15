49db8b35c116ac531e2cf9de6151ff52b3a6a09d
mori0091 <mori-d@qc4.so-net.ne.jp>
2023-03-15 18:08:03 +0900

    fix(xmem): reimplemented `vmem_dup()`
    - Reimplemented `vmem_dup()`.
    - Removed other `vmem_dup_*()` variants.

--- include/vmem_rw.h
+++ include/vmem_rw.h
@@ -75,58 +75,6 @@ void vmem_close(void);
  */
 void vmem_dup(uint16_t offset, size_t len);
 
-/**
- * `MSX2` Duplicate a byte sequence starting from the specified offset to
- * current position and later.
- *
- * Optimal version of vmem_dup().
- *
- * This function does not use built-in or user-defined buffers. Instead, it uses
- * page #2 of the main RAM (usually unused RAM in ROM cartridge applications) as
- * a work buffer. This provides better performance in terms of speed and memory
- * consumption.
- *
- * \param offset distance from the current position.
- * \param len    length in bytes.
- *
- * \pre `vmem_is_opened()`
- *
- * \note
- * Page #2 (0x8000..0xbfff) of the main RAM is used as a 16KiB buffer.
- * Therefore, at least 32 KiB of main RAM is required (Page #2 + Page #3 = 16
- * KiB + 16 KiB). Some MSX computers do not meet this requirement, but MSX2 and
- * later do. MSX1 is also acceptable if it has at least 32 KiB of RAM.
- *
- * \attention
- * While executing this function, main RAM is exposed on page #2.
- * Therefore, direct access to page #2 ROM is prohibited until the function call
- * is completed. For example, if an interrupt routine accesses page #2 without
- * using an inter-slot call, its behavior is undefined.
- */
-void vmem_dup_opt2(uint16_t offset, size_t len);
-
-/**
- * `MSX2` Duplicate a byte sequence starting from the specified offset to
- * current position and later.
- *
- * Primitive function for copying from VRAM to VRAM. The work buffer must be
- * specified by the caller.
- *
- * - Processing time depends on the size of the work buffer.
- * - Larger buffer size is better (faster).
- * - If the size is very small, this function will be very slow.
- * - The recommended size is from `256` to `1024` bytes (or more if available).
- *
- * \param offset  distance from the current position.
- * \param len     length in bytes.
- * \param buf     base address of working buffer.
- * \param buf_len capacity of the working buffer, in bytes.
- *
- * \pre `vmem_is_opened()`
- */
-void vmem_dup_b(uint16_t offset, size_t len, uint8_t * buf, size_t buf_len);
-
-
 // internal functions
 
 /**
--- src/vmem_rw.c
+++ src/vmem_rw.c
@@ -77,42 +77,134 @@ void vmem_copy_opt2(vmemptr_t src, vmemptr_t dst, uint32_t len) {
   __asm__("ei");
 }
 
-void vmem_dup_b(uint16_t offset, size_t len, uint8_t * buf, size_t buf_len) {
-  assert(vmem_is_opened());
+#include "vdp.h"
 
-  if (buf_len < offset) {
-    vmem_copy_b(vmem - offset, vmem, len, buf, buf_len);
-    vmem += len;
-    return;
-  }
-
-  vmem_read(vmem - offset, buf, offset);
-  size_t n = offset;
-  while (offset <= buf_len - n) {
-    memcpy(buf + n, buf, offset);
-    n += offset;
-  }
-  vmem_set_write_address(vmem);
-  vmem += len;
-  while (len) {
-    if (len < n) {
-      n = len;
+// \note Supports only within a 16KiB aligned area of VRAM.
+void vmem_dup__16K(uint16_t src, uint16_t dst, size_t len) {
+  assert(src < 0x4000 && dst < 0x4000 && dst + len <= 0x4000);
+  dst |= 0x4000;
+  while (len--) {
+    {
+      __asm__("di");
+      vdp_port1 = src & 255;
+      vdp_port1 = (src >> 8);
+      __asm__("ei");
+      src++;
     }
-    vmem_write_chunk(buf, n);
-    len -= n;
+    const uint8_t x = vdp_port0;
+    {
+      __asm__("di");
+      vdp_port1 = dst & 255;
+      vdp_port1 = (dst >> 8);
+      __asm__("ei");
+      dst++;
+    }
+    vdp_port0 = x;
+  }
+}
+
+// \note Supports only lower 64KiB area of VRAM
+void vmem_dup__64K_1(uint16_t offset, size_t len) {
+  assert(vmem + len <= 0x10000UL);
+  uint16_t dst = (uint16_t)(vmem & 0xffff);
+  while (len--) {
+    {
+      const uint16_t src = dst - offset;
+      __asm__("di");
+      VDP_SET_CONTROL_REGISTER(14, (uint8_t)(src >> 14));
+      vdp_port1 = src & 255;
+      vdp_port1 = (src >> 8) & 0x3f;
+      __asm__("ei");
+    }
+    const uint8_t x = vdp_port0;
+    {
+      __asm__("di");
+      VDP_SET_CONTROL_REGISTER(14, (uint8_t)(dst >> 14));
+      vdp_port1 = dst & 255;
+      vdp_port1 = ((dst >> 8) & 0x3f) | 0x40;
+      __asm__("ei");
+      dst++;
+    }
+    vdp_port0 = x;
+  }
+}
+
+// \note Supports only upper 64KiB area of VRAM
+void vmem_dup__64K_2(uint16_t offset, size_t len) {
+  assert(vmem - offset >= 0x10000UL);
+  uint16_t dst = (uint16_t)(vmem & 0xffff);
+  while (len--) {
+    {
+      const uint16_t src = dst - offset;
+      __asm__("di");
+      VDP_SET_CONTROL_REGISTER(14, (uint8_t)(src >> 14) | 4);
+      vdp_port1 = src & 255;
+      vdp_port1 = (src >> 8) & 0x3f;
+      __asm__("ei");
+    }
+    const uint8_t x = vdp_port0;
+    {
+      __asm__("di");
+      VDP_SET_CONTROL_REGISTER(14, (uint8_t)(dst >> 14) | 4);
+      vdp_port1 = dst & 255;
+      vdp_port1 = ((dst >> 8) & 0x3f) | 0x40;
+      __asm__("ei");
+      dst++;
+    }
+    vdp_port0 = x;
+  }
+}
+
+// \note Supports all 128KiB area of VRAM. (slow)
+void vmem_dup__128K(uint16_t offset, size_t len) {
+  assert(vmem + len <= 0x20000UL);
+  vmemptr_t dst = vmem;
+  while (len--) {
+    {
+      const vmemptr_t src = dst - offset;
+      __asm__("di");
+      VDP_SET_CONTROL_REGISTER(14, (uint8_t)(src >> 14));
+      vdp_port1 = src & 255;
+      vdp_port1 = (src >> 8) & 0x3f;
+      __asm__("ei");
+    }
+    const uint8_t x = vdp_port0;
+    {
+      __asm__("di");
+      VDP_SET_CONTROL_REGISTER(14, (uint8_t)(dst >> 14));
+      vdp_port1 = dst & 255;
+      vdp_port1 = ((dst >> 8) & 0x3f) | 0x40;
+      __asm__("ei");
+      dst++;
+    }
+    vdp_port0 = x;
   }
 }
 
 void vmem_dup(uint16_t offset, size_t len) {
   assert(vmem_is_opened());
-  vmem_dup_b(offset, len, tmp_buf, sizeof(tmp_buf));
-}
-
-void vmem_dup_opt2(uint16_t offset, size_t len) {
-  assert(vmem_is_opened());
-  uint8_t slot_p2 = msx_get_slot(PAGE_ADDR(2));
-  msx_ENASLT(msx_get_slot(PAGE_ADDR(3)), PAGE_ADDR(2));
-  vmem_dup_b(offset, len, PAGE_ADDR(2), PAGE_SIZE);
-  msx_ENASLT(slot_p2, PAGE_ADDR(2));
-  __asm__("ei");
+  uint16_t dst = (uint16_t)vmem & 0x3fff;
+  if (offset < 0x4000 && offset <= dst) {
+    uint16_t src = dst - offset;
+    uint16_t n = 0x4000 - dst;
+    if (len <= n) {
+      vmem_dup__16K(src, dst, len);
+      vmem += len;
+      return;
+    }
+    vmem_dup__16K(src, dst, n);
+    vmem += n;
+    len -= n;
+    // dst = 0;
+  }
+  if (vmem + len <= 0x10000UL) {
+    vmem_dup__64K_1(offset, len);
+  }
+  else if (0x10000UL <= vmem - offset) {
+    vmem_dup__64K_2(offset, len);
+  }
+  else {
+    vmem_dup__128K(offset, len);
+  }
+  vmem += len;
 }

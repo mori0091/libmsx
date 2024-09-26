f5599effe88aa8e02109771ac91d6676f8d127c9
mori0091 <mori-d@qc4.so-net.ne.jp>
2024-09-26 19:13:02 +0900

    fix: #83 CPU mode should not be implicitly changed

--- src/vmem_write.c
+++ src/vmem_write.c
@@ -18,13 +18,12 @@
 
 void vmem_write_chunk(const uint8_t * p, uint16_t n);
 
+// Please execute this on Z80 mode, if neccessary, to avoid excessive waiting
+// time inserted by S1990 though not recommended.
+// (i.e. Z80 mode is faster than R800 mode in this case!)
+// See also https://www.msx.org/wiki/VRAM_access_speed
+// and issue #83 (https://github.com/mori0091/libmsx/issues/83)
 void vmem_write(vmemptr_t dst, void* src, uint16_t len) {
   vmem_set_write_address(dst);
-  // Force Z80 mode to avoid excessive waiting time inserted by S1990.
-  // (i.e. Z80 mode is faster than R800 mode in this case!)
-  // See also https://www.msx.org/wiki/VRAM_access_speed
-  const uint8_t cpu = msx_get_cpu_mode();
-  msx_set_cpu_mode(0x00);
   vmem_write_chunk((const uint8_t *)src, len);
-  msx_set_cpu_mode(cpu);
 }
